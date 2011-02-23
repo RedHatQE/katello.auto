197afa7639cc8440f216219813d291bd6fa755c0
Jeff Weiss <jweiss@redhat.com>
2011-02-23 17:02:03 -0500

    for real this time

---
+++ src/kalpana/tests/content_providers.clj
@@ -0,0 +1,24 @@
+(ns kalpana.tests.content-providers
+  (:require [kalpana.tasks :as tasks]
+            [clojure.contrib.logging :as log])
+  (:import [org.testng.annotations Test])
+  (:use [test-clj.testng :only [gen-class-testng data-driven]]
+        [com.redhat.qe.verify :only [verify]]))
+
+(defn ^{Test {:groups ["content-providers"]}} create_simple [_]
+  (tasks/create-content-provider (tasks/timestamp "auto-cp") "my description" "http://myrepo.url.com/blah/" "Fedora" "myuser" "mypass"))
+
+(defn ^{Test {:groups ["content-providers"]}} delete_simple [_]
+  (let [cp-name (tasks/timestamp "auto-cp-delete")]
+    (tasks/create-content-provider cp-name "my description" "http://myrepo.url.com/blah/" "Fedora" "myuser" "mypass")
+    (tasks/delete-content-provider cp-name)))
+
+(comment  (defn mytest [x y z] (verify (= z (+ x y))))
+
+          (data-driven mytest {org.testng.annotations.Test {:groups ["content-providers"]}}
+                       [[1 2 3]
+                        [5 10 15]
+                        [-1 -1 -2]
+                        [1 2 4]])) 
+
+(gen-class-testng)
