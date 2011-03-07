259b47fb7202dc9885a724746e7b957fe138b76f
Jeff Weiss <jweiss@redhat.com>
2011-03-07 14:50:17 -0500

    Fedora was removed as a default CP type

--- src/kalpana/tests/content_providers.clj
+++ src/kalpana/tests/content_providers.clj
@@ -6,11 +6,11 @@
         [com.redhat.qe.verify :only [verify]]))
 
 (defn ^{Test {:groups ["content-providers"]}} create_simple [_]
-  (tasks/create-content-provider (tasks/timestamp "auto-cp") "my description" "http://myrepo.url.com/blah/" "Fedora" "myuser" "mypass"))
+  (tasks/create-content-provider (tasks/timestamp "auto-cp") "my description" "http://myrepo.url.com/blah/" "Red Hat" "myuser" "mypass"))
 
 (defn ^{Test {:groups ["content-providers"]}} delete_simple [_]
   (let [cp-name (tasks/timestamp "auto-cp-delete")]
-    (tasks/create-content-provider cp-name "my description" "http://myrepo.url.com/blah/" "Fedora" "myuser" "mypass")
+    (tasks/create-content-provider cp-name "my description" "http://myrepo.url.com/blah/" "Red Hat" "myuser" "mypass")
     (tasks/delete-content-provider cp-name)))
 
 (comment  (defn mytest [x y z] (verify (= z (+ x y))))
