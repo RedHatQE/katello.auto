80ba27944699fac088ccb7ad95d5fd9679eacaf6
Jeff Weiss <jweiss@redhat.com>
2011-05-06 11:39:33 -0400

    Revert "disable sync management tests until we can create products"
    This reverts commit 2190a4dac807cf1a3d277dfd050907d74bd0cb5b.

--- src/kalpana/tests/sync_management.clj
+++ src/kalpana/tests/sync_management.clj
@@ -5,13 +5,12 @@
         [error.handler :only [with-handlers handle ignore]]
         [com.redhat.qe.verify :only [verify]]))
 
-(comment "can't create product via automation yet, so commenting out until either ui or api allows this"
-         (defn ^{Test {:groups ["sync"]}} simple_sync [_]
-           (let [myproduct (tasks/timestamp "sync-test1")]
-             (tasks/create-content-provider myproduct  "testing sync"
-                                            "http://hudson.rhq.lab.eng.bos.redhat.com:8080/hudson/job/subscription-manager_master/lastSuccessfulBuild/artifact/rpms/x86_64"
-                                            "Generic Yum Collection")
-             (let [results (tasks/sync-products [myproduct] 60000)]
-               (verify (every? #(= "Sync complete." %) (vals results))))))
+(defn ^{Test {:groups ["sync"]}} simple_sync [_]
+  (let [myproduct (tasks/timestamp "sync-test1")]
+    (tasks/create-content-provider myproduct  "testing sync"
+                                   "http://hudson.rhq.lab.eng.bos.redhat.com:8080/hudson/job/subscription-manager_master/lastSuccessfulBuild/artifact/rpms/x86_64"
+                                   "Generic Yum Collection")
+    (let [results (tasks/sync-products [myproduct] 60000)]
+      (verify (every? #(= "Sync complete." %) (vals results))))))
 
-         (gen-class-testng))
+(gen-class-testng)
