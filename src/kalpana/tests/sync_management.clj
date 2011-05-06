c6e63021e13ad34a68f75c7a5dc36d5ffab117a2
Jeff Weiss <jweiss@redhat.com>
2011-05-06 11:41:12 -0400

    disable sync test, can't create products/repos with automation yet

--- src/kalpana/tests/sync_management.clj
+++ src/kalpana/tests/sync_management.clj
@@ -5,7 +5,9 @@
         [error.handler :only [with-handlers handle ignore]]
         [com.redhat.qe.verify :only [verify]]))
 
-(defn ^{Test {:groups ["sync"]}} simple_sync [_]
+(defn ^{Test {:groups ["sync"]
+              :enabled false  ;;no way to get products into the system yet in an automated fashion -jmw 
+              :description "Sync a product."}} simple_sync [_]
   (let [myproduct (tasks/timestamp "sync-test1")]
     (tasks/create-content-provider myproduct  "testing sync"
                                    "http://hudson.rhq.lab.eng.bos.redhat.com:8080/hudson/job/subscription-manager_master/lastSuccessfulBuild/artifact/rpms/x86_64"
