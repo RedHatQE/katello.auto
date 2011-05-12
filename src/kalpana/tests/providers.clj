5e977917af02376311139b54321d96a8886a66a9
Jeff Weiss <jweiss@redhat.com>
2011-05-12 15:13:23 -0400

    use variations in provider script

--- src/kalpana/tests/providers.clj
+++ src/kalpana/tests/providers.clj
@@ -24,10 +24,11 @@
     (tasks/verify-success #(tasks/delete-provider cp-name))))
 
 (defn validate [name description repo-url type  expected-result]
-  (let [name (if (fn? name) (name) name)]
-    (validate/field-validation (fn []
-                                 (tasks/create-provider name description type repo-url)
-                                :success) expected-result)))
+  (let [name (if (fn? name) (name) name)] ; timestamping at compile time defeats purpose of unique names
+    (validate/field-validation       
+     (fn []                           
+       (tasks/create-provider name description type repo-url) 
+       :success) expected-result)))
 
 (def vdata (vec (concat
                  [[nil "blah" "http://sdf.com" :redhat :name-cant-be-blank]
@@ -38,12 +39,13 @@
 
                   [#(tasks/timestamp "mytestcp2") "blah" nil :redhat :repository-url-cant-be-blank]
                   [#(tasks/timestamp "mytestcp3") nil "http://sdf.com" :redhat :success]
-                  [#(tasks/timestamp "mytestcp4") nil "http://sdf.com" :custom :success]
-                  [#(tasks/timestamp "mytestcp5") (validate/test-data :javascript) "http://sdf.com" :custom   :success]]
-                 (vec (for [bad-ws (validate/test-data :trailing-whitespace)]
-                        [bad-ws nil  "http://sdf.com" :custom  :name-must-not-contain-trailing-whitespace]))
-                 (vec (for [bad-inv-char (validate/test-data :invalid-character)]
-                        [bad-inv-char nil "http://sdf.com" :custom :name-must-not-contain-characters])))))
+                  [#(tasks/timestamp "mytestcp4") nil "http://sdf.com" :custom :success]]
+                 (validate/variations
+                  [#(tasks/timestamp "mytestcp5") :javascript "http://sdf.com" :custom :success])
+                 (validate/variations                  
+                  [:trailing-whitespace nil  "http://sdf.com" :custom  :name-must-not-contain-trailing-whitespace])
+                 (validate/variations
+                  [:invalid-character nil "http://sdf.com" :custom :name-must-not-contain-characters]))))
 
 (data-driven validate {Test {:groups ["providers"]}} vdata)
 
