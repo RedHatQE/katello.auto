5ac0abaf4f7f514757332b9934f30ad27e188b3b
Jeff Weiss <jweiss@redhat.com>
2011-05-19 21:09:41 -0400

    adding some product tests

--- src/kalpana/tests/providers.clj
+++ src/kalpana/tests/providers.clj
@@ -1,20 +1,33 @@
 (ns kalpana.tests.providers
   (:require [kalpana.tasks :as tasks]
             [kalpana.validation :as validate])
-  (:import [org.testng.annotations Test])
+  (:import [org.testng.annotations Test BeforeGroups])
   (:use [test-clj.testng :only [gen-class-testng data-driven]]
         [error.handler :only [with-handlers handle ignore]]
         [com.redhat.qe.verify :only [verify]]))
 
-(defn ^{Test {:groups ["providers"]}} create_simple [_]
+(def test-provider-name (atom nil))
+(def test-product-name (atom nil))
+
+(defn test-provider [type]
   (let [result-message (tasks/create-provider
                         (tasks/timestamp "auto-cp")
                         "my description"
-                        :redhat
-                        "http://myrepo.url.com/blah/")]
+                        type
+                        (if (= type :redhat)
+                          "http://myrepo.url.com/blah/" nil))]
     (verify (string? result-message))))
 
-(defn ^{Test {:groups ["providers"]}} delete_simple [_]
+(defn ^{Test {:description "Create a Red Hat provider (sunny day test)"
+              :groups ["providers"]}} create_redhat_simple [_]
+  (test-provider :redhat))
+
+(defn ^{Test {:description "Create a custom provider (sunny day test)"
+              :groups ["providers"]}} create_custom_simple [_]
+  (test-provider :custom))
+
+(defn ^{Test {:description "Delete a provider."
+              :groups ["providers"]}} delete_simple [_]
   (let [cp-name (tasks/timestamp "auto-cp-delete")]
     (tasks/create-provider
      cp-name
@@ -23,14 +36,14 @@
      "http://myrepo.url.com/blah/")
     (tasks/verify-success #(tasks/delete-provider cp-name))))
 
-(defn validate [name description repo-url type  expected-result]
+(defn validate-provider [name description repo-url type  expected-result]
   (let [name (if (fn? name) (name) name)] ; timestamping at compile time defeats purpose of unique names
     (validate/field-validation       
      (fn []                           
        (tasks/create-provider name description type repo-url) 
        :success) expected-result)))
 
-(def vdata (vec (concat
+(def provider-vdata (vec (concat
                  [[nil "blah" "http://sdf.com" :redhat :name-cant-be-blank]
                     
                   ^{Test {:groups ["blockedByBug-703528"]
@@ -47,6 +60,20 @@
                  (validate/variations
                   [:invalid-character nil "http://sdf.com" :custom :name-must-not-contain-characters]))))
 
-(data-driven validate {Test {:groups ["providers"]}} vdata)
+(data-driven validate-provider {Test {:groups ["providers"]}} provider-vdata)
+
+(defn ^{BeforeGroups {:description "Create a custom Provider to do further testing with."
+                      :groups ["providers"]
+                      :value ["products"]
+                      :dependsOnMethods ["create_custom_simple"]}}
+  create_test_custom_provider [_]
+  (tasks/create-provider (reset!  test-provider-name (tasks/timestamp ("cust")))
+                         "my description" :custom))
+
+(defn ^{Test {:groups ["providers" "products"]
+              :description "Create a product"}}
+  create_product [_]
+  (tasks/add-product @test-provider-name (reset! test-product-name (tasks/timestamp "prod"))
+                     "test product" "http://test.url" true true))
 
 (gen-class-testng)
