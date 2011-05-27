db6b58d89be919c20ee29f96e904f44a0b5933cf
Jeff Weiss <jweiss@redhat.com>
2011-05-26 22:43:01 -0400

    log POST req bodies too

--- src/kalpana/rest.clj
+++ src/kalpana/rest.clj
@@ -5,30 +5,32 @@
             [clojure.contrib.logging :as log])
   (:refer-clojure :exclude (get)))
 
-(defmacro with-logs [ & body]
-  `(let [r# (do ~@body)]
-     (log/debug (str "Result of REST call: \n" (with-out-str (pprint/pprint r#))))
-     r#))
+(defmacro with-logs [body & forms]
+  `(do (if-let [b# ~body] 
+         (log/info (str "About to make REST call with body: \n" (with-out-str (pprint/pprint b#)))))
+       (let [r# (do ~@forms)]
+         (log/debug (str "Result of REST call: \n" (with-out-str (pprint/pprint r#))))
+         r#)))
 
 (defn get
   "Gets the url, and decodes JSON in the response body, returning a
   clojure datastructure."
   [url & [req]]
-  (with-logs
+  (with-logs nil
     (-> (httpclient/get url (merge req {:accept :json})) :body json/read-json)))
 
 (defn post
   "Encodes datastructure in body to JSON, posts to url, using user and pw. "
   [url user pw body & [req]]
-  (with-logs
+  (with-logs body
     (-> (httpclient/post url (merge req {:body (json/json-str body)
-                                      :basic-auth [user pw]
-                                      :accept :json
+                                         :basic-auth [user pw]
+                                         :accept :json
                                          :content-type :json}))
         :body json/read-json)))
 
 (defn delete [url user pw & [req]]
-  (with-logs (-> (httpclient/delete url (merge req {:basic-auth [user pw]
+  (with-logs req (-> (httpclient/delete url (merge req {:basic-auth [user pw]
                                                     :accept :json
                                                     :content-type :json}))
                  :body)))
