b2cdce2ec53a5a9a23ce59a410134d958906ba7f
Jeff Weiss <jweiss@redhat.com>
2011-06-21 11:29:52 -0400

    switch sync test to use api for setup, block test

--- src/kalpana/tests/sync_management.clj
+++ src/kalpana/tests/sync_management.clj
@@ -1,5 +1,6 @@
 (ns kalpana.tests.sync_management
-  (:require [kalpana.tasks :as tasks])
+  (:require [kalpana.tasks :as tasks]
+            [kalpana.api-tasks :as api])
   (:import [org.testng.annotations Test])
   (:use [test-clj.testng :only [gen-class-testng data-driven]]
         [error.handler :only [with-handlers handle ignore]]
@@ -15,14 +16,23 @@
 (defn ^{Test {:groups ["sync"
                        "blockedByBug-705355"
                        "blockedByBug-711105"
-                       "blockedByBug-712318" ]
+                       "blockedByBug-712318"
+                       "blockedByBug-715004" ]
               :description "Sync a product."}}
   simple_sync [_]
   (let [myprovider (tasks/timestamp "sync")
         myproduct (tasks/timestamp "sync-test1")]
-    (tasks/create-provider myprovider  "provider to test syncing" :custom)
-    (tasks/add-product myprovider myproduct  "testing sync" "http://meaningless.url" true true)
-    (tasks/add-repo myprovider myproduct (tasks/timestamp "testrepo") (@config :sync-repo))
+    (api/create-provider (@config :admin-org)
+                         (@config :admin-user)
+                         (@config :admin-password)
+                         :name myprovider
+                         :description "provider to test syncing"
+                         :type "Custom")
+    (api/create-product myproduct myprovider
+                        :description "testing sync"
+                        :url "http://meaningless.url")
+    (api/create-repo (tasks/timestamp "testrepo") (@config :admin-org) myproduct
+                     (@config :sync-repo))
     (let [results (tasks/sync-products [myproduct] 60000)]
       (verify (every? #(= "Sync complete." %) (vals results))))))
 
