9df1c1920c68897de36fe9fdc637b0022bb2ef34
Jeff Weiss <jweiss@redhat.com>
2011-07-19 16:39:40 -0400

    fix bz checker to return nil if all are closed (false) instead of empty list (true)

--- src/katello/tests/suite.clj
+++ src/katello/tests/suite.clj
@@ -18,8 +18,10 @@
 
 (defn blocked-by-bz-bugs [ & ids]
   (fn [_]
-    (let [checker (BzChecker/getInstance)]
-     (filter (fn [id] (.isBugOpen checker id)) ids))))
+    (let [checker (BzChecker/getInstance)
+          still-open (filter (fn [id] (.isBugOpen checker id)) ids)]
+      (if (= 0 (count still-open)) nil
+          still-open))))
 
 (defn suite []
   {:name "startup"
