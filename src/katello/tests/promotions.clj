b9a50aad4a46a0ac2ab098fbb7ad5570bfa378c3
Jeff Weiss <jweiss@redhat.com>
2011-11-08 13:07:41 -0500

    move lazy-seq to level of each row of data-driven test

--- src/katello/tests/promotions.clj
+++ src/katello/tests/promotions.clj
@@ -61,8 +61,8 @@
                              Verifies that it shows up in the new env."}
               
              (data-driven verify-promote-content
-                          [[(lazy-seq [locker (@config :first-env)]) {:products (fn [] (set (tasks/uniqueify "MyProduct" 3)))}]
-                           [(lazy-seq [locker (@config :first-env) (@config :second-env)]) {:products (fn [] (set (tasks/uniqueify "ProductMulti" 3)))}]])
+                          [(lazy-seq [[locker (@config :first-env)] {:products (fn [] (set (tasks/uniqueify "MyProduct" 3)))}])
+                           (lazy-seq [[locker (@config :first-env) (@config :second-env)] {:products (fn [] (set (tasks/uniqueify "ProductMulti" 3)))}])])
              dep-chain)}])
 
 
