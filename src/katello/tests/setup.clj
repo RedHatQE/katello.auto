60edca887095c8b246d071798b1f0ed47c41125d
Jeff Weiss <jweiss@redhat.com>
2011-11-12 09:56:54 -0500

    left out with-admin

--- src/katello/tests/setup.clj
+++ src/katello/tests/setup.clj
@@ -27,7 +27,7 @@
   (tasks/login (@conf/config :admin-user) (@conf/config :admin-password)))
 
 (defn switch-new-admin-user [user pw]
-  (api/create-user user {:password pw })
+  (api/with-admin (api/create-user user {:password pw }))
   (tasks/assign-role {:user user
                       :roles ["Administrator"]})
   (tasks/logout)
