93eb1434ac283d253afa4e71b6996e00e4afa834
Jeff Weiss <jweiss@redhat.com>
2012-03-14 20:44:49 -0400

    fix template promotion - katello now requires products in a template to be promoted first.

--- src/katello/tests/promotions.clj
+++ src/katello/tests/promotions.clj
@@ -5,6 +5,7 @@
         [test.tree.builder :only [data-driven dep-chain]]
         [serializable.fn :only [fn]]
         [bugzilla.checker :only [open-bz-bugs]]
+        [clj-http.client :only [with-connection-pool]]
         [com.redhat.qe.verify :only [verify-that]])
   (:refer-clojure :exclude [fn]))
 
@@ -27,30 +28,47 @@
       (verify-that (every? current promoted)))))
 
 (defn verify-promote-content [envs content]
-  (let [create-repo-fn
-        (fn [prod]
-          (api/create-product prod
-                              {:provider-name @provider-name
-                               :description "test product"})
-          (let [repo-name (uniqueify "mytestrepo")]
-            (api/create-repo repo-name
-                             {:product-name prod
-                              :url "http://blah.com"})
-            repo-name))]
-    (doseq [product-name (content :products)]
-      (api/with-admin (create-repo-fn product-name)))
-    (doseq [template-name (content :templates)]
-      (api/with-admin
-        (let [product-name (uniqueify "templ-prod")
-              repo-name (create-repo-fn product-name)] 
-          (api/with-env library
-            (api/create-template {:name template-name
-                                  :description "template to be promoted"})
-            (api/add-to-template template-name {:repositories [{:product product-name
-                                                                :name repo-name}]}))))))
-  (doseq [[from-env target-env] (partition 2 1 envs)]
-    (promote-content from-env target-env content)
-    (verify-all-content-present content (environment-content target-env))))
+  (with-connection-pool {:timeout 10 :threads 1 :insecure? true}
+    (let [create-repo-fn
+          (fn [prod]
+            (let [product-id (-> prod
+                                (api/create-product
+                                 {:provider-name @provider-name
+                                  :description "test product"})
+                                :id)
+                  repo-name (uniqueify "mytestrepo")]
+              (api/create-repo repo-name
+                               {:product-name prod
+                                :url "http://blah.com"})
+              {:repo-name repo-name
+               :product-id product-id}))]
+      (doseq [product-name (content :products)]
+        (api/with-admin (create-repo-fn product-name)))
+      (doseq [template-name (content :templates)]
+        (api/with-admin
+          (let [product-name (uniqueify "templ-prod")
+                {:keys [product-id repo-name]} (create-repo-fn product-name)] 
+            (doseq [to-env (drop 1 envs)]
+              (api/with-env to-env
+                (api/promote {:products [{:product_id product-id}]} )))
+            (api/with-env library
+              (api/create-template {:name template-name
+                                    :description "template to be promoted"})
+              (api/add-to-template template-name {:repositories [{:product product-name
+                                                                  :name repo-name}]}))))))
+    (doseq [[from-env target-env] (partition 2 1 envs)]
+      (promote-content from-env target-env content)
+      (verify-all-content-present content (environment-content target-env)))))
+
+(def promo-data
+  [(fn [] [[library (@config :first-env)]
+          {:products (set (take 3 (unique-names "MyProduct")))}])
+   (fn [] [[library (@config :first-env)
+           (@config :second-env)]
+          {:products (set (take 3 (unique-names "ProductMulti")))}])
+   (fn [] [[library (@config :first-env)
+           (@config :second-env)]
+          {:templates (set (take 3 (unique-names "TemplateMulti")))}])])
 
 (def tests
   [{:configuration true
@@ -63,14 +81,7 @@
         :description "Takes content and promotes it thru more environments.
                             Verifies that it shows up in the new env."}
               
-       (data-driven [(fn [] [[library (@config :first-env)]
-                            {:products (set (take 3 (unique-names "MyProduct")))}])
-                     (fn [] [[library (@config :first-env)
-                             (@config :second-env)]
-                            {:products (set (take 3 (unique-names "ProductMulti")))}])
-                     (fn [] [[library (@config :first-env)
-                             (@config :second-env)]
-                            {:templates (set (take 3 (unique-names "TemplateMulti")))}])])
+       (data-driven promo-data)
        dep-chain)}])
 
 
