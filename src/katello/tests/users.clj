af5065e3ca840b21ba41c9da92c3e150e2c24681
Jeff Weiss <jweiss@redhat.com>
2012-05-02 20:00:22 -0400

    fix test names

--- src/katello/tests/users.clj
+++ src/katello/tests/users.clj
@@ -15,37 +15,37 @@
 
 (def all-user-tests
 
-  (deftest "create a user"
+  (deftest "Admin creates a user"
     (create-user       (uniqueify "autouser")   user-details)
 
 
-    (deftest "edit a user"
+    (deftest "Admin changes a user's password"
       (with-unique username "edituser"
         (create-user    username                user-details)
         (edit-user      username                {:new-password "changedpwd"})))
 
   
-    (deftest "delete a user"
+    (deftest "Admin deletes a user"
       (with-unique username "deleteme"
         (create-user    username                user-details)
         (delete-user    username)))
 
 
-    (deftest "duplicate user disallowed"
+    (deftest "Two users with the same username is disallowed"
       (verify-2nd-try-fails-with       :name-taken-error     create-user    (uniqueify "dupeuser")    user-details))
 
 
-    (deftest "users' miniumum password length enforced"
+    (deftest "User's minimum password length is enforced"
       (expect-error-on-action :password-too-short  create-user (uniqueify "insecure-user") {:password "abcd", :email "me@my.org"}))
 
   
-    (deftest "search for user"
+    (deftest "Search for a user"
       (with-unique username "searchuser"
         (create-user     username                user-details)
         (validate-search :users                  {:criteria "search"})))
 
   
-    (deftest "assign role to user"
+    (deftest "Admin assigns a role to user"
       (with-unique username "autouser"
         (create-user     username                user-details)
         (assign-role     {:user username, :roles ["Administrator"]})))))
