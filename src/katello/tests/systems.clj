2c543f29e5a2db14839cfbecb82543595c73fd2b
Jeff Weiss <jweiss@redhat.com>
2012-05-05 18:27:07 -0400

    Refactored system tests

--- src/katello/tests/systems.clj
+++ src/katello/tests/systems.clj
@@ -1,73 +1,83 @@
 (ns katello.tests.systems
   (:refer-clojure :exclude [fn])
-  (:use [katello.tasks :exclude [create-activation-key]]
+  (:use katello.tasks
+        test.tree.script
         [serializable.fn :only [fn]]
         [katello.conf :only [config *environments*]]
         [com.redhat.qe.verify :only [verify-that]])
   (:require (katello [api-tasks :as api]
-                     [validation :as val]
-                     [tasks :as tasks])))
+                     [validation :as val])))
 
-(defn with-freshly-registered-system [f]
-  (f (api/with-admin
-       (api/with-env (first *environments*)
-         (api/create-system (uniqueify "newsystem")
-                            {:facts (api/random-facts)})))))
+;; Functions
 
-(def create-env 
-  (fn [] (api/with-admin
-          (api/ensure-env-exist (first *environments*) {:prior library}))))
+(defn create-test-environment [] 
+  (def test-environment (first *environments*))
+  (api/with-admin
+    (api/ensure-env-exist test-environment {:prior library})))
 
-(def rename
-  (fn []
-    (with-freshly-registered-system
-      (fn [sys]
-        (let [new-name (uniqueify "yoursys")] 
-          (edit-system (:name sys) :new-name new-name)
-          (navigate :named-systems-page {:system-name new-name}))))))
+(defn register-new-test-system [env]
+  (api/with-admin
+    (api/with-env env
+      (api/create-system (uniqueify "newsystem")
+                         {:facts (api/random-facts)}))))
 
-(def in-env
-  (fn []
-    (with-freshly-registered-system
-      (fn [sys]
-        (navigate :named-system-environment-page
-                  {:env-name (first *environments*)
-                   :system-name (:name sys)})
-        (verify-that (= (:environment_id sys)
-                        (api/with-admin
-                          (api/get-id-by-name :environment (first *environments*)))))))))
+(defn verify-system-rename [system]
+  (with-unique [new-name "yoursys"] 
+    (edit-system (:name system) :new-name new-name)
+    (navigate :named-systems-page {:system-name new-name})))
 
-(def subscribe
-  (fn []
-    (let [provider-name (uniqueify "subscr-prov")
-          product-name (uniqueify "subscribe-me")]
+(defn verify-system-appears-on-env-page
+   [system env]
+  (navigate :named-system-environment-page
+            {:env-name env
+             :system-name (:name system)})
+  (verify-that (= (:environment_id system)
+                  (api/with-admin
+                    (api/get-id-by-name :environment test-environment)))))
+
+;; Tests
+
+
+(defgroup all-system-tests
+  :group-setup create-test-environment
+
+  
+  (deftest "Rename an existing system" 
+    (-> test-environment
+       register-new-test-system
+       verify-system-rename))
+
+  
+  (deftest "Verify system appears on Systems By Environment page in its proper environment"
+    (verify-system-appears-on-env-page (register-new-test-system)))
+
+  
+  (deftest "Subscribe a system to a product"
+    (with-unique [provider-name "subscr-prov"
+                  product-name "subscribe-me"]
       (api/with-admin
         (api/create-provider provider-name)
-        (api/create-product product-name
-                            {:provider-name provider-name}))
-      (with-freshly-registered-system
-        (fn [sys]
-          (subscribe-system {:system-name (:name sys)
-                             :products [product-name]}))))))
+        (api/create-product product-name {:provider-name provider-name}))
+      (subscribe-system {:system-name (:name (register-new-test-system))
+                         :products [product-name]})))
 
-(def create-activation-key
-  (fn []
-    (tasks/create-activation-key {:name (uniqueify "auto-key")
-                                  :description "my description"
-                                  :environment (first *environments*)})))
+  
+  (deftest "Create an activation key" 
+    (create-activation-key {:name (uniqueify "auto-key")
+                            :description "my description"
+                            :environment test-environment})
 
-(def remove-activation-key
-  (fn []
-    (let [ak-name (uniqueify "auto-key-deleteme")]
-      (tasks/create-activation-key {:name ak-name
-                                    :description "my description"
-                                    :environment (first *environments*)} )
-      (delete-activation-key ak-name))))
-
-(def activation-key-dupe-disallowed
-  (fn []
-    (val/duplicate-disallowed katello.tasks/create-activation-key
-                              [{:name (uniqueify "auto-key")
+    
+    (deftest "Remove an activation key"
+      (with-unique [ak-name "auto-key-deleteme"]
+        (create-activation-key {:name ak-name
                                 :description "my description"
-                                :environment (first *environments*)}])))
+                                :environment test-environment} )
+        (delete-activation-key ak-name)))
+
+    
+    (deftest "activation-key-dupe-disallowed"
+      (val/duplicate-disallowed create-activation-key [{:name (uniqueify "auto-key")
+                                                        :description "my description"
+                                                        :environment test-environment}]))))
 
