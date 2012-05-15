5838529df601f15339ac0e8acfa880fed627210d
Jeff Weiss <jweiss@redhat.com>
2012-05-15 13:56:35 -0400

    add example properties file

---
+++ automation-properties.clj
@@ -0,0 +1,9 @@
+;; Example configuration.  Edit this file and copy it to your home
+;; directory, where it will be read when automation starts.
+;; You should specify at least the :server-url.
+;;
+;; For other options and defaults, see src/katello/conf.clj
+
+{
+ :server-url "https://my.server.hostname.com/katello/"
+}
