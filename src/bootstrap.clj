bb32692731d045a2aa13e99f0357beae9c7fe4ec
Jeff Weiss <jweiss@redhat.com>
2012-05-21 10:08:58 -0400

    update bootstrap file to be more rerunnable

--- src/bootstrap.clj
+++ src/bootstrap.clj
@@ -1,27 +1,38 @@
-(ns bootstrap)
-(do         
-   (do (require 'katello.ui-tasks :reload-all)
-       (require 'katello.conf :reload)
-       (require 'katello.setup :reload)
-       (require 'katello.client :reload)
-       (katello.conf/init)
-       
-       (import [org.openqa.selenium.server RemoteControlConfiguration SeleniumServer])
-       (com.redhat.qe.tools.SSLCertificateTruster/trustAllCerts)
-       (com.redhat.qe.tools.SSLCertificateTruster/trustAllCertsForApacheXMLRPC)
+(ns user)
+(import [org.openqa.selenium.server RemoteControlConfiguration SeleniumServer])
+(use 'katello.ui-tasks)
+(require 'katello.conf)
+(require 'katello.setup)
 
-       (let [rcconf (doto (RemoteControlConfiguration.)
-                      (.setPort 4444)
-                      (.setTrustAllSSLCertificates true))
-             selserver (SeleniumServer. rcconf)]
-         (.start selserver))        
+(declare selenium-server)
 
-       
-       (when katello.conf/*clients*
-         (katello.client/connect (katello.client/new-runner
-                                  (first katello.conf/*clients*)
-                                  "root" nil
-                                  (@katello.conf/config :client-ssh-key)
-                                  (@katello.conf/config :client-ssh-key-passphrase))))) ;;<-here for api only
-   (katello.setup/new-selenium (-> katello.conf/config deref :selenium-browsers first) true)
-   (katello.setup/start-selenium))
+;;------------
+
+(defn new-selenium-server []
+  (let [rcconf (doto (RemoteControlConfiguration.)
+                 (.setPort 4444)
+                 (.setTrustAllSSLCertificates true))]
+    (when selenium-server
+      (.stop selenium-server))
+    (def ^:dynamic selenium-server (SeleniumServer. rcconf))
+    (.start selenium-server)))
+
+(defn new-browser []
+  (katello.setup/new-selenium (-> katello.conf/config deref :selenium-browsers first) true)
+  (katello.setup/start-selenium)) 
+
+;;-------------
+
+(katello.conf/init)
+(com.redhat.qe.tools.SSLCertificateTruster/trustAllCerts)
+(com.redhat.qe.tools.SSLCertificateTruster/trustAllCertsForApacheXMLRPC)
+
+(when katello.conf/*clients*
+  (require 'katello.client)
+  (katello.client/connect (katello.client/new-runner
+                           (first katello.conf/*clients*)
+                           "root" nil
+                           (@katello.conf/config :client-ssh-key)
+                           (@katello.conf/config :client-ssh-key-passphrase)))) ;;<-here for api only
+(new-selenium-server)
+(new-browser)
