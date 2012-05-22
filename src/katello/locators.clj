02db1b0e20d8ae40fa0e555c4b23945f8667ac68
Jeff Weiss <jweiss@redhat.com>
2012-05-22 17:24:12 -0400

    Fix navigation for systems, by-environment was being confused with all.

--- src/katello/locators.clj
+++ src/katello/locators.clj
@@ -429,9 +429,8 @@
        [:new-system-template-page [] (browser click :new-template)]]]
      [:systems-tab [] (browser mouseOver :systems)
       [:systems-all-page [] (browser clickAndWait :all)
-       [:systems-by-environment-page [] (browser clickAndWait :by-environments)
-        [:system-subscriptions-page [system-name] (choose-left-pane (left-pane-item system-name))
-         [:named-systems-page [] (browser click :details)]]]]
+       [:system-subscriptions-page [system-name] (choose-left-pane (left-pane-item system-name))
+         [:named-systems-page [] (browser click :details)]]]
       [:activation-keys-page [] (browser clickAndWait :activation-keys)
        [:named-activation-key-page [activation-key-name]
         (choose-left-pane (left-pane-item activation-key-name))]
