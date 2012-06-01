7ea5706634f9e3b6225b890c2e3b332d15939efc
Jeff Weiss <jweiss@redhat.com>
2012-06-01 12:17:57 -0400

    revert auto-bootstrap - if it fails the repl will not start.  now have to type (load "bootstrap") again

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
   :dev-dependencies [[lein-autodoc "0.9.0"]]
   :checksum-deps false
   :jvm-opts ["-Xmx192m" "-Xms96m"]
-  :repl-init bootstrap
+  ;; :repl-init bootstrap
   :repositories {"my-clojars" {:url "http://clojars.org/repo"
                                :snapshots {:update :always}}
                  "my-central" {:url "http://repo1.maven.org/maven2"
