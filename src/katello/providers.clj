550aad72e351b17784bef53054b71ec8e009b7ec
Jeff Weiss <jweiss@redhat.com>
2012-12-07 14:07:48 -0500

    Clean up providers

--- src/katello/providers.clj
+++ src/katello/providers.clj
@@ -8,45 +8,43 @@
 ;; Locators
 
 (swap! ui/uimap merge
-       {:new-provider                        "new"
-        :provider-name-text                  "provider[name]"
-        :provider-description-text           "provider[description]"
-        :provider-repository-url-text        "provider[repository_url]"
-        :provider-cert-text                  (ui/textbox "provider[certificate_attributes][contents]")
-        :provider-create-save                "provider_submit"
-        :remove-provider                     (ui/link "Remove Provider")
-        :subscriptions                       (ui/link "Subscriptions")
-        :import-manifest                     "new"
-        :redhat-provider-repository-url-text "provider[repository_url]"
-        :choose-file                         "provider_contents"
-        :upload                              "upload_form_button"
-        :force-import-checkbox               "force_import"
-        :products-and-repositories           "//nav[contains(@class,'subnav')]//a[contains(.,'Products')]"
-        
-        ;;add product
-        :add-product                         (ui/button-div "Add Product")
-        :create-product                      "//input[@value='Create']"
-        :product-name-text                   "//*[@name='product[name]']"
-        :product-label-text                  "//*[@name='product[label]']"
-        :product-description-text            "//*[@name='product[description]']"
-        :remove-product                      (ui/link "Remove Product")
-        ;;add repo
-        :repo-name-text                      "repo[name]"
-        :repo-label-text                     "repo[label]"
-        :repo-url-text                       "repo[feed]" 
-        :save-repository                     "//input[@value='Create']"
-        :remove-repository                   (ui/link "Remove Repository")
-        :repo-gpg-select                     "//select[@id='repo_gpg_key']"
+       {::new                       "new"
+        ::name-text                 "provider[name]"
+        ::description-text          "provider[description]"
+        ::repository-url-text       "provider[repository_url]"
+        ::cert-text                 (ui/textbox "provider[certificate_attributes][contents]")
+        ::create-save               "provider_submit"
+        ::provider                  (ui/link "Remove Provider")
+        ::subscriptions             (ui/link "Subscriptions")
+        ::import-manifest           "new"
+        ::choose-file               "provider_contents"
+        ::upload                    "upload_form_button"
+        ::force-import-checkbox     "force_import"
+        ::products-and-repositories "//nav[contains(@class,'subnav')]//a[contains(.,'Products')]"
+        ::subscriptions-items       "//table[@id='redhatSubscriptionTable']/tbody/tr"}
 
-        ;;redhat page
-        :subscriptions-items                 "//table[@id='redhatSubscriptionTable']/tbody/tr"
+       ;;products
+       {::add-product              (ui/button-div "Add Product")
+        ::create-product           "//input[@value='Create']"
+        ::product-name-text        "//*[@name='product[name]']"
+        ::product-label-text       "//*[@name='product[label]']"
+        ::product-description-text "//*[@name='product[description]']"
+        ::remove-product           (ui/link "Remove Product")}
 
-        ;;Package Filters
-        :create-new-package-filter                (ui/link "+ New Filter")
-        :new-package-filter-name                  "filter[name]"
-        :new-package-filter-description           "filter[description]"
-        :save-new-package-filter                  "filter_submit"
-        :remove-package-filter-key                (ui/link "Remove Filter")})
+       ;;repos
+       {::repo-name-text    "repo[name]"
+        ::repo-label-text   "repo[label]"
+        ::repo-url-text     "repo[feed]" 
+        ::save-repository   "//input[@value='Create']"
+        ::remove-repository (ui/link "Remove Repository")
+        ::repo-gpg-select   "//select[@id='repo_gpg_key']"}
+
+       ;;Package Filters
+       {::create-new-package-filter      (ui/link "+ New Filter")
+        ::new-package-filter-name        "filter[name]"
+        ::new-package-filter-description "filter[description]"
+        ::save-new-package-filter        "filter_submit"
+        ::remove-package-filter-key      (ui/link "Remove Filter")})
 
 (sel/template-fns
  {add-repository "//div[@id='products']//div[contains(.,'%s')]/..//div[normalize-space(.)='Add Repository' and contains(@class, 'button')]"})
@@ -55,46 +53,46 @@
  :content-tab
  [:repositories-tab [] (sel/browser mouseOver :repositories)
   [:custom-content-repositories-page [] (sel/browser clickAndWait :custom-content-repositories)
-   [:new-provider-page [] (sel/browser click :new-provider)]
-   [:named-provider-page [provider-name] (nav/choose-left-pane  provider-name)
-    [:provider-products-repos-page [] (sel/->browser (click :products-and-repositories)
-                                                     (sleep 2000))
-     [:named-product-page [product-name] (sel/browser click (ui/editable product-name))]
-     [:named-repo-page [product-name repo-name] (sel/browser click (ui/editable repo-name))]]
-    [:provider-details-page [] (sel/browser click :details)]
-    [:provider-subscriptions-page [] (sel/browser click :subscriptions)]]]
-  [:redhat-repositories-page [] (sel/browser clickAndWait :red-hat-repositories)]  
-  [:package-filters-page [] (sel/browser clickAndWait :package-filters)
-   [:new-package-filter-page [] (sel/browser click :create-new-package-filter)]
-   [:named-package-filter-page [package-filter-name] (nav/choose-left-pane  package-filter-name)]]])
+   [::new-page [] (sel/browser click ::new)]
+   [::named-page [provider-name] (nav/choose-left-pane  provider-name)
+    [::products-repos-page [] (sel/->browser (click ::products-and-repositories)
+                                             (sleep 2000))
+     [::named-product-page [product-name] (sel/browser click (ui/editable product-name))]
+     [::named-repo-page [product-name repo-name] (sel/browser click (ui/editable repo-name))]]
+    [::details-page [] (sel/browser click :details)]
+    [::subscriptions-page [] (sel/browser click ::subscriptions)]]]
+  [::redhat-repositories-page [] (sel/browser clickAndWait :red-hat-repositories)]  
+  [::package-filters-page [] (sel/browser clickAndWait :package-filters)
+   [::new-package-filter-page [] (sel/browser click ::create-new-package-filter)]
+   [::named-package-filter-page [package-filter-name] (nav/choose-left-pane  package-filter-name)]]])
 
 ;; Tasks
 
 (defn create
   "Creates a custom provider with the given name and description."
   [{:keys [name description]}]
-  (nav/go-to :new-provider-page)
-  (fill-ajax-form {:provider-name-text name
-                   :provider-description-text description}
-                  :provider-create-save)
+  (nav/go-to ::new-page)
+  (fill-ajax-form {::name-text name
+                   ::description-text description}
+                  ::create-save)
   (notification/check-for-success {:match-pred (notification/request-type? :prov-create)}))
 
 (defn add-product
   "Adds a product to a provider, with the given name and description."
   [{:keys [provider-name name description]}]
-  (nav/go-to :provider-products-repos-page {:provider-name provider-name})
-  (browser click :add-product)
-  (sel/fill-ajax-form {:product-name-text name
-                   :product-description-text description}
-                  :create-product)
+  (nav/go-to ::products-repos-page {:provider-name provider-name})
+  (browser click ::add-product)
+  (sel/fill-ajax-form {::product-name-text name
+                       ::product-description-text description}
+                      ::create-product)
   (notification/check-for-success {:match-pred (notification/request-type? :prod-create)}))
 
 (defn delete-product
   "Deletes a product from the given provider."
   [{:keys [name provider-name]}]
-  (nav/go-to :named-product-page {:provider-name provider-name
-                                  :product-name name})
-  (browser click :remove-product)
+  (nav/go-to ::named-product-page {:provider-name provider-name
+                                   :product-name name})
+  (browser click ::remove-product)
   (browser click :confirmation-yes)
   (notification/check-for-success {:match-pred (notification/request-type? :prod-destroy)}))
 
@@ -102,46 +100,46 @@
   "Adds a repository under the given provider and product. Requires a
    name and url be given for the repo."
   [{:keys [provider-name product-name name url]}]
-  (nav/go-to :provider-products-repos-page {:provider-name provider-name})
+  (nav/go-to ::products-repos-page {:provider-name provider-name})
   (browser click (add-repository product-name))
-  (sel/fill-ajax-form {:repo-name-text name
-                   :repo-url-text url}
-                  :save-repository)
+  (sel/fill-ajax-form {::repo-name-text name
+                       ::repo-url-text url}
+                      ::save-repository)
   (notification/check-for-success {:match-pred (notification/request-type? :repo-create)}))
 
 (defn add-repo-with-key
   "Adds a repository under the given provider and product. Requires a
    name and url be given for the repo."
   [{:keys [provider-name product-name name url gpgkey]}]
-  (nav/go-to :provider-products-repos-page {:provider-name provider-name})
+  (nav/go-to ::products-repos-page {:provider-name provider-name})
   (browser click (add-repository product-name))
-  (browser select :repo-gpg-select gpgkey)
-  (sel/fill-ajax-form {:repo-name-text name
-                   :repo-url-text url}
-                  :save-repository)
+  (browser select ::repo-gpg-select gpgkey)
+  (sel/fill-ajax-form {::repo-name-text name
+                       ::repo-url-text url}
+                      ::save-repository)
   (notification/check-for-success {:match-pred (notification/request-type? :repo-create)}))
 
 (defn delete-repo
   "Deletes a repository from the given provider and product."
   [{:keys [name provider-name product-name]}]
-  (nav/go-to :named-repo-page {:provider-name provider-name
-                               :product-name product-name
-                               :repo-name name})
-  (browser click :remove-repository)
+  (nav/go-to ::named-repo-page {:provider-name provider-name
+                                :product-name product-name
+                                :repo-name name})
+  (browser click ::remove-repository)
   (browser click :confirmation-yes)
   (notification/check-for-success {:match-pred (notification/request-type? :repo-destroy)}))
 
 (defn delete
   "Deletes the named custom provider."
   [name]
-  (nav/go-to :named-provider-page {:provider-name name})
-  (browser click :remove-provider)
+  (nav/go-to ::named-page {:provider-name name})
+  (browser click ::provider)
   (browser click :confirmation-yes)
   (notification/check-for-success {:match-pred (notification/request-type? :prov-destroy)}))
 
 (defn edit
   "Edits the named custom provider. Takes an optional new name, and
   new description." [{:keys [name new-name description]}]
-  (nav/go-to :provider-details-page {:provider-name name})
-  (in-place-edit {:provider-name-text new-name
-                  :provider-description-text description}))
+  (nav/go-to ::details-page {:provider-name name})
+  (in-place-edit {::name-text new-name
+                  ::description-text description}))
