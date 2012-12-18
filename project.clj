921fca57b0be2c330be07219e8a834bcd01389b7
Jeff Weiss <jweiss@redhat.com>
2012-12-18 15:10:44 -0500

    increase heap size

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                  [org.clojure/tools.cli "0.2.1"]
                  [bugzilla.checker "0.1.2-SNAPSHOT"]
                  [fn.trace "1.3.2.0-SNAPSHOT"]]
-  :jvm-opts ["-Xmx384m" "-Xms48m"]
+  :jvm-opts ["-Xmx768m" "-Xms48m"]
   :repositories {"my-clojars" {:url "http://clojars.org/repo"
                                :snapshots {:update :always}}
                  "my-central" {:url "http://repo1.maven.org/maven2"
