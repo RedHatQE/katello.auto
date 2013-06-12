299b5f96e17e346a0bf2cf495d42541cb81f256d
Kedar Bidarkar <kbidarka@redhat.com>
2013-06-13 00:34:02 +0530

    Added some locators required for distributors

--- src/katello/distributors.clj
+++ src/katello/distributors.clj
@@ -1,6 +1,7 @@
 (ns katello.distributors
   (:require [com.redhat.qe.auto.selenium.selenium :as sel :refer [browser]]
             [katello :as kt]
+            [clojure.data :as data]
             (katello [ui :as ui]
                      [rest :as rest]
                      [navigation :as nav]
@@ -15,18 +16,34 @@
   {::new                   "new"
    ::create                "commit"
    ::distributor-name-text "distributor[name]"
-   ::remove-link           (ui/remove-link "distributors")})
+   ::details-link          "distributor_details"
+   ::keyname-text          "new_custom_info_keyname"
+   ::value-text            "new_custom_info_value"
+   ::custom-info-button    "//input[@id='create_custom_info_button']"
+   ::remove-link           (ui/remove-link "distributors")
+   ::distributor-info-link (ui/third-level-link "distributor_info")
+   ::events-history-link   (ui/third-level-link "distributor_events")
+   ::custom-info-link      (ui/third-level-link "custom_info")
+   ::save-button           "//button[@type='submit']"
+   ::cancel-button         "//button[@type='cancel']"})
+
+(sel/template-fns
+ {value-text             "custom_info[%s]"})
 
 ;; Nav
 
 (nav/defpages :katello.deployment/any katello.menu
   [::page
-   [::named-page (fn [distributor] (nav/choose-left-pane distributor))]
+   [::named-page (fn [distributor] (nav/choose-left-pane distributor))
+    [::details-menu (nav/browser-fn (mouseOver ::details-link))
+     [::distributor-info-page (nav/browser-fn (click ::custom-info-link))]
+     [::events-history-page (nav/browser-fn (click ::custom-info-link))]
+     [::custom-info-page (nav/browser-fn (click ::custom-info-link))]]]
    [::new-page (nav/browser-fn (click ::new))]])
 
 ;; Tasks
 
-(defn create
+(defn- create
   "Creates a new distributor with the given name and environment."
   [{:keys [name env]}]
   {:pre [(instance? katello.Environment env)]}
@@ -36,7 +53,7 @@
                       ::create)
   (notification/check-for-success {:match-pred (notification/request-type? :distributor-create)}))
 
-(defn delete
+(defn- delete
   "Deletes the named distributor."
   [dist]
   {:pre [(instance? katello.Distributor dist)]}
@@ -45,9 +62,27 @@
   (browser click ::ui/confirmation-yes)
   (notification/check-for-success {:match-pred (notification/request-type? :distributor-destroy)}))
 
+(defn- update
+  "Updates the named distributor."
+  [dist updated]
+  {:pre [(instance? katello.Distributor dist)]}
+  (let [[to-remove {:keys [name env keyname value]
+                    :as to-add} _] (data/diff dist updated)]
+    (when (and keyname value)
+      (nav/go-to ::custom-info-page dist)
+      (sel/->browser 
+        (setText ::keyname-text keyname)
+        (setText ::value-text value)
+        (keyUp ::keyname-text "z")
+        (click ::custom-info-button)))))
+        
+      
+      
+
 (extend katello.Distributor
   ui/CRUD {:create create
-           :delete delete}
+           :delete delete
+           :update* update}
   rest/CRUD (let [id-url (partial rest/url-maker [["api/distributors/%s" [identity]]])
                   query-urls (partial rest/url-maker [["api/environments/%s/distributors" [:env]]
                                                       ["api/organizations/%s/distributors" [(comp :org :env)]]])]
