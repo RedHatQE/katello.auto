b485ca2689f63ce31738fd5cfd58c622fd6da523
Kedar Bidarkar <kbidarka@redhat.com>
2013-06-13 00:35:03 +0530

    Update custom info for a distributor

--- src/katello/tests/distributors.clj
+++ src/katello/tests/distributors.clj
@@ -4,6 +4,9 @@
             (katello [tasks :refer :all]
                      [rest :as rest]
                      [ui :as ui]
+                     [navigation :as nav]
+                     [ui-common :as common]
+                     [notifications :as notification]
                      [distributors :as distributor])
             (test.tree [script :refer [defgroup deftest]])
             [serializable.fn :refer [fn]]
@@ -23,4 +26,20 @@
       (let [dist1 (assoc dist :env (kt/newEnvironment {:name "Library" :org org}))]
         (rest/create org)
         (ui/create dist1)
-        (ui/delete dist1)))))
+        (ui/delete dist1))))
+  
+  (deftest "Update custom info for a distributor"   
+    :data-driven true
+    (fn [input-loc new-value save?]
+      (with-unique [org (kt/newOrganization {:name "auto-org"})
+                    env (kt/newEnvironment {:name "environment" :org org})
+                    dist (kt/newDistributor {:name "test-dist" :env env})]
+        (let [expected-res #(-> % :type (= :success))]
+          (ui/create-all (list org env dist))
+          (ui/update dist assoc :keyname "fname" :value "redhat")
+          (expecting-error expected-res
+            (nav/go-to ::distributor/custom-info-page dist)              
+            (common/save-cancel ::distributor/save-button ::distributor/cancel-button input-loc new-value save?)))))
+    
+    [[(distributor/value-text "fname") "fedora" false]
+     [(distributor/value-text "fname") "Schrodinger's cat" true]]))
