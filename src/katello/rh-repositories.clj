15865f8f5c5d8036da3ca122b12f5b5ef82b0111
Kedar Bidarkar <kbidarka@redhat.com>
2013-06-21 18:11:54 +0530

    Added Selection support for all repo types, rpms, srpms, debug, beta

--- src/katello/rh-repositories.clj
+++ src/katello/rh-repositories.clj
@@ -10,9 +10,9 @@
 ;; Locators
 
 (sel/template-fns
- {expand-product  "//div[@id='rpms']//td[contains(@style,'cursor') and contains(.,'%s')]/span"
-  select-repo-set "//div[@id='rpms']//span[@class='expander_area' and contains(.,'%s')]/../../td/input[@type='checkbox']"
-  expand-repo-set  "//div[@id='rpms']//span[@class='expander_area' and contains(.,'%s')]/span"
+ {expand-product  "//div[@id='%s']//td[contains(@style,'cursor') and contains(.,'%s')]/span"
+  select-repo-set "//div[@id='%s']//span[@class='expander_area' and contains(.,'%s')]/../../td/input[@type='checkbox']"
+  expand-repo-set  "//div[@id='%s']//span[@class='expander_area' and contains(.,'%s')]/span"
   select-repo     "//td[contains(.,'%s')]/../td/label/input[@type='checkbox']"})
 
 (ui/defelements :katello.deployment/any []
@@ -34,7 +34,7 @@
    [::product-isos-page (nav/browser-fn (click ::isos-tab))] 
    [::others-page (nav/browser-fn (click ::others-tab))]])
 
-#_(def repo-map
+(def repo-map
   {:rpms    ::rpms-page
    :srpms   ::source-rpms-page
    :debug   ::debug-rpms-page
@@ -43,30 +43,34 @@
 (def red-hat-provider (kt/newProvider {:name "Red Hat" :org katello.conf/*session-org*}))
 
 (def cloudforms (kt/newProduct {:name "Red Hat CloudForms" :provider red-hat-provider}))
-(def system-engine (kt/newRHRepoSet {:name "Red Hat CloudForms System Engine", :product cloudforms}))
+(def system-engine (kt/newRedHatRepoSet {:name "Red Hat CloudForms System Engine", :product cloudforms}))
 
 (def rhel (kt/newProduct {:name "Red Hat Enterprise Linux Server" :provider red-hat-provider}))
-(def rhel-cftools (kt/newRHRepoSet {:name "Red Hat CloudForms Tools for RHEL 6" :product rhel}))
+(def rhel-cftools (kt/newRedHatRepoSet {:name "Red Hat CloudForms Tools for RHEL 6" :product rhel}))
                                     
 (def rh-repos (concat (for [reponame ["Red Hat CloudForms System Engine RPMs x86_64 6.4"
                                       "Red Hat CloudForms System Engine RPMs x86_64 6Server"]]
-                          (kt/newRHRepo {:name reponame, :reposet system-engine}))
-                        (for [reponame ["Red Hat CloudForms Tools for RHEL 6 RPMs i386 6.4"
-                                        "Red Hat CloudForms Tools for RHEL 6 RPMs i386 6Server"
-                                        "Red Hat CloudForms Tools for RHEL 6 RPMs x86_64 6.4"
-                                        "Red Hat CloudForms Tools for RHEL 6 RPMs x86_64 6Server"]]
-                          (kt/newRHRepo {:name reponame, :reposet rhel-cftools}))))
+                        (kt/newRedHatRepo {:name reponame, :reposet system-engine, 
+                                           :type "rpms", :deselect? false}))
+                      (for [reponame ["Red Hat CloudForms Tools for RHEL 6 RPMs i386 6.4"
+                                      "Red Hat CloudForms Tools for RHEL 6 RPMs i386 6Server"
+                                      "Red Hat CloudForms Tools for RHEL 6 RPMs x86_64 6.4"
+                                      "Red Hat CloudForms Tools for RHEL 6 RPMs x86_64 6Server"]]
+                        (kt/newRedHatRepo {:name reponame, :reposet rhel-cftools, 
+                                           :type "rpms", :deselect? false}))))
 
-(defn enable-redhat-repos
+(defn enable-disable-redhat-repos
   "Enable the given list of rh-repos in the current org."
   [rh-repos]
   (doseq [repo rh-repos]
-    (nav/go-to ::rpms-page (kt/org repo))
-    (let [prd (kt/product repo)
-          reposet (kt/reposet repo)
-          checked? (common/disabled? (select-repo-set (:name reposet)))]
-      (browser click (expand-product (:name prd)))
+    (nav/go-to (repo-map (keyword (repo :type))) (kt/org repo))
+    (let [prd      (kt/product repo)
+          reposet  (kt/reposet repo)
+          checked? (common/disabled? (select-repo-set (:type repo) (:name reposet)))]
+      (browser click (expand-product (:type repo) (:name prd)))
       (if-not checked?
-        (browser check (select-repo-set (:name reposet)))
-        (browser click (expand-repo-set (:name reposet))))
-      (browser check (select-repo (:name repo))))))
+        (browser check (select-repo-set (:type repo)(:name reposet)))
+        (browser click (expand-repo-set (:type repo)(:name reposet))))
+      (if (repo :deselect?)
+        (browser uncheck (select-repo (:name repo)))
+        (browser check (select-repo (:name repo)))))))
