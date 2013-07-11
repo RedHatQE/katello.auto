578b02c7a764e2df7b5962eca8eafaf14ed0554c
Jeff Weiss <jweiss@redhat.com>
2013-07-11 13:13:42 -0400

    Add some docstrings and bugfixes
    * Do client setup (rpm install, etc) at provision time, not when client is consumed - speeds up prep
    * Some bugfixes for shutting down queue
    * Some safety to prevent deadlocking

--- src/katello/client/provision.clj
+++ src/katello/client/provision.clj
@@ -9,28 +9,52 @@
 
 (defonce queue (atom nil))
 
-(defn new-queue [capacity]
+(defn new-queue
+  "Create a new instance queue with given capacity."
+  [capacity]
   (Queue. (ArrayBlockingQueue. capacity) nil))
 
 (defn- clean-up-queue [q]
   (let [leftovers (java.util.ArrayList.)]
     (.drainTo q leftovers)
-    (cloud/unprovision-all (map deref leftovers))))
+    (cloud/unprovision-all (->> leftovers
+                                (map deref)
+                                (take-while #(not= :end %))))))
+
+(defn- add-ssh "Add ssh command runner field to the given instance."
+  [inst]
+  (try
+    (assoc inst :ssh-connection
+           (client/new-runner (cloud/ip-address inst)))
+    (catch Exception e
+      (assoc inst :ssh-connection-error e))))
+
+(defn- provision
+  "Provision a client from deltacloud and set it up as a
+   client (including installation of rpm to configure rhsm)"
+  [def]
+  (let [inst (->> def
+                  (cloud/provision conf/*cloud-conn*)
+                  add-ssh)]
+    (client/setup-client (:ssh-connection inst) (:name inst))
+    inst))
 
 (defn fill-queue
+  "Continuously backfill the queue (qa should be an atom containing a
+  Queue), will block indefinitely until the atom is updated with the
+  field :shutdown? set to true.  At that point, the queue will be
+  drained and all instances in it unprovisioned.  This function should
+  probably be called with future, because it blocks."
   [qa]
   (loop [defs (conf/client-defs "pre-provision"), p (promise)]  
     (let [shutdown? (:shutdown? @qa)
           accepted? (.offer (:queue @qa) p 5 TimeUnit/SECONDS)]
-      (cond shutdown? (do (deliver p (cloud/map->Instance {})) ; avoid undelivered promise
+      (cond shutdown? (do (deliver p :end) ; avoid undelivered promise
                           (clean-up-queue (:queue @qa)))
-            accepted? (do (future (try
-                                    (->> defs
-                                         first
-                                         (cloud/provision conf/*cloud-conn*)
-                                         (deliver p))
-                                    (catch Exception e
-                                      (deliver p e))))
+            accepted? (do (future (try+
+                                    (->> defs first provision (deliver p))
+                                    (catch Object o
+                                      (deliver p o))))
                           (recur (rest defs) (promise))) ; new promise
             :else (recur defs p)) ; recur with same args if queue is full
       )))
@@ -39,19 +63,10 @@
   "Takes a Queue and returns the next instance from it, checking for
   errors and throwing them if they occurred."
   [q]
-  (let [inst (-> q :queue .poll deref)]
-    (if (instance? Throwable inst)
-      (throw+ inst)
-      inst)))
-
-(defn add-ssh [inst]
-  (try
-    (assoc inst :ssh-connection
-           (client/new-runner (cloud/ip-address inst)))
-    (catch Exception e
-      (assoc inst :ssh-connection-error e))))
-
-
+  (let [inst (-> q :queue .poll (deref 600000 {:type ::dequeue-timeout}))]
+    (if (instance? deltacloud.Instance inst)
+      inst
+      (throw+ inst))))
 
 (defmacro with-n-clients
   "Provisions n clients with instance name basename (plus unique
@@ -97,12 +112,11 @@
   `(let [inst# (-> queue
                    deref                ; the atom
                    dequeue) 
-         ~ssh-conn-bind (client/new-runner (cloud/ip-address inst#))]
-     (client/setup-client ~ssh-conn-bind (:name inst#))
+         ~ssh-conn-bind (:ssh-connection inst#)]
      (try ~@body
           (finally
             (when cloud/*kill-instance-when-finished*
-              (future cloud/unprovision inst#))))))
+              (future (cloud/unprovision inst#)))))))
 
 (defn init "Initialize the queue to n items" [n]
   (reset! queue (new-queue n))
