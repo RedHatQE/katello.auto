5861b354eb9cf52ed274caca99665502f06eb4f3
Jeff Weiss <jweiss@redhat.com>
2013-07-24 09:13:53 -0400

    Block all system tests

--- src/katello/tests/systems.clj
+++ src/katello/tests/systems.clj
@@ -113,7 +113,7 @@
 
 (defgroup system-tests
   :group-setup create-test-environment
-  :blockers (bz-bugs "717408" "728357")
+  :blockers (bz-bugs "717408" "728357" "987925")
 
   (deftest "Rename an existing system"
     :uuid "50895adf-ae72-5dd4-bd1b-1baf59fd0633"
