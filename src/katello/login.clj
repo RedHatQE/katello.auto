beb319dfc488d3c940141399706188472fb92d89
Blair Morrison <blairmorrison@gmail.com>
2013-08-23 11:53:48 -0400

    SAM 1.3 changes to login and rest

--- src/katello/login.clj
+++ src/katello/login.clj
@@ -17,8 +17,8 @@
    ::re-log-in-link    "//a[contains(@href, '/login')]"
    ::error-message     "//ul[@class='error']"
    ::close-error       "//div[@id='notifications']//div[@class='control']"
-   ::interstitial      "//a[contains(@class,'menu-item-link') and contains(.,'Select an Organization')]"}
-  )
+   ;; ::interstitial      "//a[contains(@class,'menu-item-link') and contains(.,'Select an Organization')]"
+   ::interstitial      "//div[contains(@id,'interstitial') and contains(.,'Choose an Organization')]"})
 
 (defn logged-in?
   "Returns true if the browser is currently showing a page where a
@@ -64,22 +64,22 @@
   ([] (login *session-user* {:org *session-org*}))
   ([{:keys [name password] :as user} & [{:keys [org default-org]}]]
      (when (logged-in?) (logout))
-     (when (browser/exists? ::re-log-in-link)
+     #_(when (browser/exists? ::re-log-in-link)
        (browser/click ::re-log-in-link))
 
-     (when (signo-error?)
+     #_(when (signo-error?)
        (clear-signo-errors))
 
      (wd/->browser (clear ::username-text)
                    (clear ::password-text))
      (browser/quick-fill-submit {::username-text name}
-                             {::password-text password}
-                             {::log-in browser/click})
+                                {::password-text password}
+                                {::log-in browser/click})
      ;; throw errors
-     ;;(notification/verify-no-error)     ; katello notifs
-     ;;(notification/flush)
+     (notification/verify-no-error)     ; katello notifs
+     (notification/flush)
      
-     (if (signo-error?)                 ; signo notifs
+     #_(if (signo-error?)                 ; signo notifs
        (throw+ (list (ui/map->Notification {:level :error
                                             :notices (list (browser/text ::error-message))}))))
      ;; no interstitial for signo logins, if we go straight to default org, and that's the
