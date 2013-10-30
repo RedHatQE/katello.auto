b6ccf2cea21f9259345dec8cd5e97a16805e31f1
sghai <sghai@redhat.com>
2013-10-30 14:35:19 +0530

    Fixed locator & subscribe fn for attach/remove subscriptions

--- src/katello/systems.clj
+++ src/katello/systems.clj
@@ -18,7 +18,7 @@
 ;; Locators
 
 (browser/template-fns
- {subscription-available-checkbox "//div[@id='panel-frame']//table[@id='subscribeTable']//td[contains(normalize-space(.),'%s')]//input[@type='checkbox']"
+ {subscription-available-checkbox "//td[contains(normalize-space(.),'%s')]/preceding-sibling::td[@class='row-select']/input[@type='checkbox']"
   subscription-current-checkbox   "//div[@id='panel-frame']//table[@id='unsubscribeTable']//td[contains(normalize-space(.),'%s')]//input[@type='checkbox']"
   checkbox                        "//input[@class='system_checkbox' and @type='checkbox' and parent::td[normalize-space(.)='%s']]"
   sysgroup-checkbox               "//input[@title='%s']"
@@ -113,6 +113,8 @@
 
    ;;subscriptions pane
    ::subscriptions               "//nav[@class='details-navigation']//li/a[contains (text(), 'Subscriptions')]"
+   ::attach-subscription         "//button[@ng-click='attachSubscriptions()']"
+   ::remove-subscription         "//button[@ng-click='removeSubscriptions()']"
    ::red-subs-icon               "//i[@class='icon-circle red']"})
 
 ;; Nav
@@ -195,8 +197,8 @@
                            (browser/click (checkbox-fn (:name item))))
                          (browser/click submit)
                          (notification/success-type :sys-update-subscriptions)))]
-    (sub-unsub-fn add-products subscription-available-checkbox ::subscribe)
-    (sub-unsub-fn remove-products subscription-current-checkbox ::unsubscribe)))
+    (sub-unsub-fn add-products subscription-available-checkbox ::attach-subscription)
+    (sub-unsub-fn remove-products subscription-current-checkbox ::remove-subscription)))
 
 (defn- edit-system-name
   [{:keys [name]}]
